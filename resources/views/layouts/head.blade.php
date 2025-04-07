d53b6af70046c02a2814dc54c8e8754591936fa0
nabilwafi <nabilwafi88@gmail.com>
2025-04-07 11:59:40 +0700

    finalisasi

--- resources/views/layouts/head.blade.php
+++ resources/views/layouts/head.blade.php
@@ -1,3 +1,4 @@
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link rel="shortcut icon" type="image/ico" href="{{asset('LOGO PUTIH.png')}}" />
 <title>@yield('title', 'Mazer - Admin Dashboard')</title>
